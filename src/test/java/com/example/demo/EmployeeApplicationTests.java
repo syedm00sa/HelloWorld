c922f73deec4eff1216b6178c862b955ae915207
syedm00sa <suhailsyedmoosa@gmail.com>
2023-08-24 16:45:57 +0530

    Fixed error

--- src/test/java/com/example/demo/EmployeeApplicationTests.java
+++ src/test/java/com/example/demo/EmployeeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmployeeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
--- src/test/java/com/example/restservice/RestserviceApplicationTests.java
+++ src/test/java/com/example/restservice/RestserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
